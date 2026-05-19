fc2ae01448d2b8a053ab276963ce13b2e931dc3d
Trung Lê <trungrua951@gmail.com>
2026-05-19 12:40:09 +0700

    feat: impl retry service

---
+++ retry-service/src/main/java/com/dev/retryservice/RetryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dev.retryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetryserviceApplication.class, args);
+	}
+
+}
